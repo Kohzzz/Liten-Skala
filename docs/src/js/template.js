ad4061429539d6681dbb80216ede242c16d314d6
kohzzz <kohzzz28@gmail.com>
2021-01-24 13:46:31 +0900

    add fooder

--- docs/src/js/template.js
+++ docs/src/js/template.js
@@ -10,3 +10,16 @@ function header(rootDir){
         }
     });
 }
+
+function header(rootDir){
+    $.ajax({
+        url: rootDir + "fooder.html",
+        cache: false,
+        async: false,
+        dataType: "html",
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.write(html);
+        }
+    });
+}
